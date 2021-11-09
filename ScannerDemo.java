7024162a23eb70e16d1ef1632ac696eaeb0f902d
katee.cooper2@gmail.com <kate cooper>
2021-11-09 16:44:18 +0000

    added ScannerDemo.java

---
+++ ScannerDemo.java
@@ -0,0 +1,28 @@
+package com.qa.qacommunity;
+
+public class ScannerDemo {
+	public static double answer;
+	public static double addition(double num1, double num2) {
+		double answer = num1 + num2;
+		return answer;
+		
+	}
+	
+	public static double subtraction(double num1, double num2) {
+		double answer = num1 - num2;
+		return answer;
+	}
+	
+	
+	public static double multiplication(double num1, double num2) {
+		double answer = num1 * num2;
+		return answer;
+		
+	}
+	
+	public static double division(double num1, double num2) {
+		double answer = num1/num2;
+		return answer;
+	}
+
+}
