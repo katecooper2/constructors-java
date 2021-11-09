44d9a19f6ae5f618ca8d9a26ce70672c0f114cd5
katee.cooper2@gmail.com <kate cooper>
2021-11-09 16:43:28 +0000

    added Runner3.java

---
+++ Runner3.java
@@ -0,0 +1,29 @@
+package com.qa.qacommunity;
+import java.util.Scanner;
+
+public class Runner3 {
+
+	public static void main(String[] args) {
+		
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Please select a method");
+		
+		String method = scan.nextLine();
+		
+		System.out.println("Okay. Now using " + method + ". Please enter your first value.");
+		
+		String num1 = scan.nextLine();
+		
+		System.out.println("And now your second value");
+		
+		String num2 = scan.nextLine();
+		
+		System.out.println("okay calculating...");
+		
+		
+		
+
+		
+	}
+}
