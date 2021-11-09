f0ea551cf974b743071e19f2c30bc3c4743c675e
katee.cooper2@gmail.com <kate cooper>
2021-11-09 16:46:29 +0000

    added Menu.java

---
+++ Menu.java
@@ -0,0 +1,22 @@
+package com.qa.qacommunity;
+
+import java.util.Scanner;
+
+public class Menu {
+	
+ Scanner input = new Scanner(System.in);
+ 
+ public void menu() {
+	boolean menuFlag = true;
+	String menuOption = "";
+	int num1;
+	int num2;
+	
+	
+	
+	
+	
+	
+ }
+	
+}
